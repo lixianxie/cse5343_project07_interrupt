84ef8eeb99f9e62ec0c979cb94dc1e2c9c6061df
lixianxie <lxie@smu.edu>
2013-11-06 08:44:02 -0600

    adding printSring.

--- kernel.c
+++ kernel.c
@@ -1,17 +1,15 @@
 //lixian xie
-//a simple kernel that displays a message.
+//write some simple system calls.
 
-
+void printString(char* str){
+	int i;
+	for(i=0;str[i]!='\0';i++){
+		interrupt(0x10,0xE*+str[i],0,0,0);	
+	}	
+}
 
 int main(){
-	char str[100]="Hello World";
-	int i=0;
-	while(str[i]!='\0'){
-		putInMemory(0xB000,32768+2*i,str[i]); //32768=0x8000
-		putInMemory(0xB000,32769+2*i,0x7);
-		i++;
-	}
-
+	printString("Hello World");	
 	while(1){
 		//todo
 	}
