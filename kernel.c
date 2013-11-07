a27d30d6dae7f3414f0e446a142d1a96f520b701
lixianxie <lxie@smu.edu>
2013-11-06 18:27:46 -0600

    print hello world.

--- kernel.c
+++ kernel.c
@@ -1,17 +1,31 @@
 //lixian xie
 //write some simple system calls.
 
-void printString(char* str){
-	int i;
-	for(i=0;str[i]!='\0';i++){
-		interrupt(0x10,0xE*+str[i],0,0,0);	
-	}	
-}
+void printString(char* str);
+void readString(char* str);
 
 int main(){
-	printString("Hello World");	
+ 	printString("Hello World");	
 	while(1){
 		//todo
 	}
 	return 0;
 }
+
+void printString(char* str){
+	int i;
+	for(i=0;str[i]!=0;i++){
+		interrupt(0x10,0xE*256+str[i],0,0,0);	
+	}	
+}
+
+void readString(char* str){
+	int i;
+	for(i=0;;i++){
+		interrupt(0x16,0,0,0,0);	
+	}
+	
+}
+
+
+
