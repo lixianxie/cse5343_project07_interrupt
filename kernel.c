85453e3677b32a6dfd8956896e4eac0e41e591c5
lixianxie <lxie@smu.edu>
2013-11-08 17:41:39 -0600

    did a little revision.

--- kernel.c
+++ kernel.c
@@ -52,9 +52,11 @@ void readString(char* str){
 	tempChar=interrupt(0x16,0,0,0,0);
 	while(tempChar!=0xd){
 		interrupt(0x10,0xE*256+tempChar,0,0,0);
-		if(tempChar!=0x8)str[i]=tempChar;		
-		tempChar=interrupt(0x16,0,0,0,0);
-		i++;
+		if(tempChar!=0x8){
+			str[i]=tempChar;		
+			tempChar=interrupt(0x16,0,0,0,0);
+			i++;
+		}else tempChar=interrupt(0x16,0,0,0,0);
 	}
 	str[i]=0xa;	   //jump to next line
 	str[i+1]=0xd;  //return to head of current line
